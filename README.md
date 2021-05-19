b973b595c58e1a5e03fd976503481e7b74dae9fb
Riley Shahar <riley.shahar@gmail.com>
2021-05-18 22:34:16 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a library for collecting data and doing stats on competitive debate tournaments. It currently isn't suitable for use, at least until it ratelimits itself.
+
+## License
+
+Licensed under the GNU GPL v3 license. See [LICENSE](LICENSE) for more.
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! A library for doing stats on competitive debate tournaments.
+#![doc(html_root_url = "https://docs.rs/debate-stats/0.0.1")]
+#![warn(rust_2018_idioms, missing_docs, missing_debug_implementations)]
+#![deny(
+    broken_intra_doc_links,
+    invalid_codeblock_attributes,
+    private_intra_doc_links
+)]
