5e68b7c4933b8c1491514fc03795c8644ab2557d
Riley Shahar <riley.shahar@gmail.com>
2021-05-31 01:06:43 -0700

    Impl `get_api_url`
    We apparently only need the URL by reference for scraping tabroom, so I
    propagated up the call stack. It's a little inelegant, but oh well.

--- src/tournament/from_url.rs
+++ src/tournament/from_url.rs
@@ -4,19 +4,20 @@ use std::convert::{TryFrom, TryInto};
 
 use crate::util::network;
 use crate::{Error, Result, Tournament};
+use network::process_url;
 use url::Url;
 
 mod tabroom;
 
 impl Tournament {
     pub(super) fn from_url_impl(url_str: &str) -> Result<Self> {
-        let url = network::process_url(url_str)?;
+        let url = process_url(url_str)?;
         match (&url).try_into()? {
-            TabHost::Tabroom => Self::from_tabroom(url),
+            TabHost::Tabroom => Self::from_tabroom(&url),
         }
     }
 
-    fn from_tabroom(url: Url) -> Result<Self> {
+    fn from_tabroom(url: &Url) -> Result<Self> {
         Self::from_tabroom_impl(url)
     }
 }
@@ -49,7 +50,7 @@ mod tests {
     use super::*;
 
     fn get_tabhost_from_str(url: &str) -> Result<TabHost> {
-        (&network::process_url(url)?).try_into()
+        (&process_url(url)?).try_into()
     }
 
     #[test]
--- src/tournament/from_url/tabroom.rs
+++ src/tournament/from_url/tabroom.rs
@@ -4,14 +4,15 @@
 
 use crate::util::network;
 use crate::{Error, Result, SearchingFor, Tournament};
+use network::{fetch_url, process_url};
 use reqwest::blocking::Response;
 use url::Url;
 
 impl Tournament {
-    pub(super) fn from_tabroom_impl(url: Url) -> Result<Self> {
-        let api_url = get_api_url(&url)?;
+    pub(super) fn from_tabroom_impl(url: &Url) -> Result<Self> {
+        let api_url = get_api_url(url)?;
         // TODO: Should we call with a client here?
-        let resp = network::fetch_url(api_url, None)?;
+        let resp = fetch_url(api_url, None)?;
         let xml = get_xml_body(resp)?;
         process_api(xml)
     }
@@ -22,7 +23,12 @@ impl Tournament {
 /// # Errors
 /// If the tournament ID can't be resolved from the URL.
 fn get_api_url(url: &Url) -> Result<Url> {
-    todo!();
+    let id = get_id(url)?;
+    let api_url_str = format!(
+        "https://www.tabroom.com/api/tourn_published.mhtml?tourn_id={}",
+        id
+    );
+    process_url(&api_url_str)
 }
 
 /// Get the tournament's ID from any associated URL.
@@ -54,12 +60,22 @@ mod tests {
     use super::*;
 
     #[test]
+    fn get_api_url_works() -> Result<()> {
+        assert_eq!(
+            get_api_url(
+                &(process_url("https://www.tabroom.com/index/tourn/index.mhtml?tourn_id=17253"))?
+            )?
+            .as_str(),
+            "https://www.tabroom.com/api/tourn_published.mhtml?tourn_id=17253"
+        );
+        Ok(())
+    }
+
+    #[test]
     fn get_id_works() -> Result<()> {
         assert_eq!(
             get_id(
-                &(network::process_url(
-                    "https://www.tabroom.com/index/tourn/index.mhtml?tourn_id=17253"
-                ))?
+                &(process_url("https://www.tabroom.com/index/tourn/index.mhtml?tourn_id=17253"))?
             )?,
             17253
         );
@@ -70,7 +86,7 @@ mod tests {
     fn get_id_works_pariings() -> Result<()> {
         assert_eq!(
             get_id(
-                &(network::process_url(
+                &(process_url(
                     "https://www.tabroom.com/index/tourn/postings/round.mhtml?tourn_id=17253&round_id=622046"
                 ))?
             )?,
@@ -83,9 +99,7 @@ mod tests {
     fn get_id_works_jack_howe() -> Result<()> {
         assert_eq!(
             get_id(
-                &(network::process_url(
-                    "https://www.tabroom.com/index/tourn/index.mhtml?tourn_id=16814"
-                ))?
+                &(process_url("https://www.tabroom.com/index/tourn/index.mhtml?tourn_id=16814"))?
             )?,
             16814
         );
@@ -94,7 +108,7 @@ mod tests {
 
     #[test]
     fn get_id_errors_base_tabroom() -> Result<()> {
-        let err = get_id(&(network::process_url("https://tabroom.com"))?).unwrap_err();
+        let err = get_id(&(process_url("https://tabroom.com"))?).unwrap_err();
         assert!(matches!(err, Error::HtmlParseFailed(SearchingFor::ID)));
         Ok(())
     }
