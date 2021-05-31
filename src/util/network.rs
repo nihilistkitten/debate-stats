b19307c3af0e8a45ef0b890c363a28c02e4bc2c6
Riley Shahar <riley.shahar@gmail.com>
2021-05-31 00:11:44 -0700

    Impl network::process_url
    This implementation is fairly straightforward, but I think correct. The
    tests should be sufficient for our use-cases.

--- src/util/network.rs
+++ src/util/network.rs
@@ -1,15 +1,44 @@
 //! Utilities related to networking.
 
-use crate::Result;
+use crate::{Error, Result};
 use reqwest::blocking::{Client, Response};
 use url::Url;
 
 /// Turn a url string into a Url.
-pub fn process_url(_url_str: &str) -> Result<Url> {
-    todo!();
+pub fn process_url(url: &str) -> Result<Url> {
+    Url::parse(url).map_err(|e| Error::url_conversion(e, url))
 }
 
 /// Fetch a url.
 pub fn fetch_url(_url: Url, _client: Option<Client>) -> Result<Response> {
     todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn process_url_works_base_only() -> Result<()> {
+        let test_str = "https://example.org/";
+        let url = process_url(test_str)?;
+        assert_eq!(url.as_str(), test_str);
+        Ok(())
+    }
+
+    #[test]
+    fn process_url_works_long_url() -> Result<()> {
+        let test_str = "https://www.tabroom.com/index/tourn/index.mhtml?tourn_id=17253";
+        let url = process_url(test_str)?;
+        assert_eq!(url.as_str(), test_str);
+        Ok(())
+    }
+
+    #[test]
+    fn process_url_error_invalid_url() {
+        let invalid_url = "abcdef";
+        let err = process_url(invalid_url).unwrap_err();
+        assert!(matches!(err, Error::UrlConversion { .. }));
+        assert!(err.to_string().contains(invalid_url));
+    }
 }
